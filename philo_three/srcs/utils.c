4884bfd0f9651a6a54febf3ee3acf429e737b949
Melchior MAJ <mmaj@e2r5p10.42.fr>
2021-03-05 10:42:34 +0100

    philo_two done

---
+++ philo_three/srcs/utils.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmaj <mmaj@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/02 10:28:45 by mmaj              #+#    #+#             */
+/*   Updated: 2021/03/02 10:29:24 by mmaj             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+int			ft_atoi(char *str)
+{
+	long			i;
+	long			sign;
+	unsigned int	nb;
+
+	i = 0;
+	sign = 1;
+	nb = 0;
+	while (str[i] == 32 || str[i] == '\t' || str[i] == '\n' ||
+	str[i] == '\r' || str[i] == '\v' || str[i] == '\f')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = sign * -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nb = nb * 10;
+		nb = nb + str[i] - 48;
+		i++;
+	}
+	return ((int)(nb * sign));
+}
+
+int			ft_isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	return (0);
+}
